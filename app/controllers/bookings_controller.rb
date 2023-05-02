b7eb984f0f6e5f8c525a367c9e8bfb7fd6404b5b
robertli <robertg.li@yahoo.com>
2023-05-01 22:52:14 -0400

    adding some changes to mvc and validations

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,26 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @toy = Toy.find(params[:toy_id])
+    @booking.user = current_user
+    @booking.toy = @toy
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @toy = Toy.find(params[:toy_id])
+    @booking.user = current_user
+    @booking.toy = @toy
+    if @booking.save
+      redirect_to toy_path(@toy)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:toy_rating, :user_rating, :toy_review, :user_review, :start_date, :end_date)
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :toy
+
+  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+  validates :start_date, presence: true
 end
