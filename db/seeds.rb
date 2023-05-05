312d3511d223fc6a7ccfc7feb099a730b3eb11f7
Emre Ozen <fossoyeuse@proton.me>
2023-05-05 19:10:47 -0400

    dealt with the image upload issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,45 +13,45 @@ Booking.delete_all
 Toy.delete_all
 User.delete_all
 
-puts "Loading the image..."
-file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
-toy = Toy.new(name: "NES", description: "A great console", price: 100.00)
-toy.photos.attach(io: file, filename: "nes.png", content_type: "image/png")
-toy.save
-
-puts"Create new users..."
+puts "Create new users..."
 5.times do
-    user = User.create!(
-        email: "#{Faker::Music.chord}@gmail.com",
-        password: '1fowest1'
-    )
-    60.times do
+  user = User.create!(
+    email: "#{Faker::Music.chord}@gmail.com",
+    password: '1fowest1'
+  )
+  60.times do
 
-    end
+  end
 end
 users = User.all
 puts "#{User.count} users created"
 puts "Creating the Toys..."
 prices = (1..100).to_a
 100.times do
-    Toy.create!(
-        name: Faker::Games::Pokemon.name,
-        price: prices.sample.to_f,
-        user: users.sample,
-        description: "test"
-    )
+  Toy.create!(
+    name: Faker::Games::Pokemon.name,
+    price: prices.sample.to_f,
+    user: users.sample,
+    description: "test"
+  )
 end
 toys = Toy.all
 puts "#{toys.count} toys created"
-puts"Creating Bookings..."
+puts "Creating Bookings..."
 10.times do
   date = Faker::Date.between(from: '2021-09-23', to: '2022-09-25')
   Booking.create!(
-      start_date: date,
-      end_date: date + 1,
-      toy: toys.sample,
-      user: users.sample
+    start_date: date,
+    end_date: date + 1,
+    toy: toys.sample,
+    user: users.sample
   )
 end
 bookings = Booking.all
 puts "#{bookings.count} bookings created"
+
+puts "Loading the image..."
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+toy = Toy.create!(name: "NES", description: "A great console", price: 100.00, user: users.sample)
+toy.photos.attach(io: file, filename: "nes.png", content_type: "image/png")
+#toy.save
